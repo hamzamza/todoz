275272f40727f3200d20bad0c9032df8f3628201
abdelouahedGit <abdeouahedfanan@gmail.com>
2023-03-09 22:21:37 +0100

    changement

---
+++ frontend/lib/widgets/DrawerList.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class DrawList extends StatefulWidget {
+  const DrawList({Key? key}) : super(key: key);
+
+  @override
+  State<DrawList> createState() => _DrawListState();
+}
+
+class _DrawListState extends State<DrawList> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ frontend/lib/widgets/HeaderDrawer.dart
@@ -0,0 +1,17 @@
+
+import 'package:flutter/material.dart';
+class HeaderDrawer extends StatefulWidget {
+  const HeaderDrawer({Key? key}) : super(key: key);
+
+  @override
+  State<HeaderDrawer> createState() => _HeaderDrawerState();
+}
+
+class _HeaderDrawerState extends State<HeaderDrawer> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
